6ed8a05fac8e1a259442b3b159a814c441207834
FletCHGM <daved.vh82@gmail.com>
2023-05-27 22:54:45 +0300

    Переделал логику пагинации

--- lib/features/model/image_hero.dart
+++ lib/features/model/image_hero.dart
@@ -1,17 +1,10 @@
 import '../Home/widgets/widgets_view.dart';
 
 class ImageHero {
-  pickImageList(int page) async {
-    List paths = await FirebaseImagePicker().getFirebaseImagesPaths(page);
-    List URLs = await FirebaseImagePicker().getFirebaseImagesURLs(paths);
-    return URLs;
-  }
-
-  imageHero(BuildContext context, int page) async {
+  imageHero(BuildContext context, List URLs) {
     List<Hero> miniHeroList = [];
     List<Hero> heroList = [];
     String image;
-    List URLs = await pickImageList(page);
     if (URLs.isEmpty) {
       List<Hero> emptyList = [];
       return emptyList;
